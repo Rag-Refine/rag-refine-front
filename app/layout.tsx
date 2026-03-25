325d5172dd7dc6ac60420f87500b3ced8f53c878
Pedro Santo <pedro.bb.90@gmail.com>
2026-03-25 09:29:25 +0000

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-background text-on-surface selection:bg-primary/30">
         {children}
         <Toaster theme="dark" position="bottom-right" />
+        <Analytics/>
       </body>
     </html>
   );
